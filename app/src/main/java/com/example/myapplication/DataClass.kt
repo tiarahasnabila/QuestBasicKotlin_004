2fe25903ee1b2a07715a113c578ecb58782dbeb5
tiarahasnabila <tiarahasnabila@gmail.com>
2024-10-11 16:18:59 +0700

    menambahkan data class untuk menyimpan data pengguna

---
+++ app/src/main/java/com/example/myapplication/DataClass.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+// Data class adalah class yang digunakan untuk menyimpan data.
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString().
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
