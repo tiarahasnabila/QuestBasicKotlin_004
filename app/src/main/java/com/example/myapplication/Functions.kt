87b28253dd1a66945ae615bb69f2077663ec690e
tiarahasnabila <tiarahasnabila@gmail.com>
2024-10-11 16:26:12 +0700

    Menambahkan fungsi tanpa parameter dan fungsi dengan parameter untuk menyapa pengguna.

---
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
