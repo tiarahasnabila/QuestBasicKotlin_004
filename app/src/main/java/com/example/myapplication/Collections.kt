132a3e9ffcd1f9f99a5e3bc328d4912ee741243a
tiarahasnabila <tiarahasnabila@gmail.com>
2024-10-11 15:09:48 +0700

    membuat fungsi contohlist, dan menyetak teks list sebagai judul

---
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+//List adalah Kumpulan data yang disusun secara berurutan dari
+//List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+//List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+//List Read-Only menggunakan listOf
+//List Mutable menggunakan mutableListOf
+
+fun ContohList() {
+    println("==== List ====")
+    // List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+}
