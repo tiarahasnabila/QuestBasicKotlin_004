aa92b79d7383a6d0351ea28c4f6926d60504b483
tiarahasnabila <tiarahasnabila@gmail.com>
2024-10-11 16:27:02 +0700

    Menambahkan fungsi dengan named arguments untuk menyapa pengguna dengan nama dan usia.

--- app/src/main/java/com/example/myapplication/Functions.kt
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -10,3 +10,11 @@ fun withParameter(name: String) {
     println("== withParameter ==")
     println("Hello, $name!")
 }
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
