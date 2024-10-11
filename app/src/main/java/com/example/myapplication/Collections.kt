86b88faef6f2872339bfb3f6292fd3b77a6e0cd8
tiarahasnabila <tiarahasnabila@gmail.com>
2024-10-11 15:47:41 +0700

    menambah fungsi baru, kemudian menandakan bahwa ini sifatnya read only dan sifatnya mutable

--- app/src/main/java/com/example/myapplication/Collections.kt
+++ app/src/main/java/com/example/myapplication/Collections.kt
@@ -55,4 +55,33 @@ fun ContohSet() {
     // Set Read-Only
     val shapesLocked: Set<String> = shape
     println(shapesLocked)
-}
+
+    fun ContohMap() {
+        println("==== Map ====")
+
+        // Map Read-Only
+        val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+        println(readOnlyShape)
+
+        // Map Mutable
+        val shape: MutableMap<String, Int> =
+            mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+        println(shape)
+
+        // Menambahkan data ke dalam Map Mutable
+        shape["Rectangle"] = 4
+        println(shape)
+
+        // Menghapus data dari Map Mutable
+        shape.remove("Circle")
+        println(shape)
+
+        // Mengubah data di dalam Map Mutable
+        shape["Square"] = 5
+        println(shape)
+
+        // Map Read-Only
+        val shapesLocked: Map<String, Int> = shape
+        println(shapesLocked)
+    }
+    }
