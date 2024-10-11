bd7d831bf27ef1dd6fe0b0212d1826fb904fb912
tiarahasnabila <tiarahasnabila@gmail.com>
2024-10-11 16:28:26 +0700

    Menambahkan fungsi dengan nilai parameter default dan fungsi untuk menghitung luas.

--- app/src/main/java/com/example/myapplication/Functions.kt
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -18,3 +18,15 @@ fun withNamedArgument(name: String, age: Int) {
     println("== withNamedArgument ==")
     println("Hello, $name! You are $age years old.")
 }
+
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = " Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withReturn(panjang : Int, lebar : Int) : Int {
+    return (panjang * lebar)
+}
